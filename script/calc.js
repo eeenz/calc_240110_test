73088045cece9afa31b29776624c1aeca9420f22
eunji <dmswl7954@gmail.com>
2024-01-10 09:52:45 +0900

    체중계 변수 등록

---
+++ script/calc.js
@@ -0,0 +1,5 @@
+const user_height = document.querySelector('#height');
+const user_weight = document.querySelector('#weight');
+const goals = document.querySelector('#goals');
+const result_btn = document.querySelector('#result');
+console.log(user_height, user_weight, goals, result_btn);
